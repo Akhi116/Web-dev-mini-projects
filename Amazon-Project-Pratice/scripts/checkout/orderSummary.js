25a8bcd1fff780837e1b773bd3c06bde937ad4b0
Akhil <akhilsadhanala116@gmail.com>
2023-12-16 13:26:21 +0530

    create payment summary

--- Amazon-Project-Pratice/scripts/checkout/orderSummary.js
+++ Amazon-Project-Pratice/scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import { calculateCartQuantity, cart, removeFromCart, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import {getProduct } from "../../data/products.js";
 import { formatCurrency } from '../utils/money.js'; // Named Export having {}
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; //default export only one thing from a file
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions,getDeliveryOption } from '../../data/deliveryOptions.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -10,28 +10,15 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-
-    let matchingProduct;
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption =  getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
     const dateString = deliveryDate.format('dddd, MMMM D');
-
 
     cartSummaryHTML += `
   <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
