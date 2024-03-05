b7e437c1feec7a1b17d0aaf123cd06f1fc0e085a
Akhil <akhilsadhanala116@gmail.com>
2024-03-05 16:12:52 +0530

    Recaptcha Js modified

--- Recaptcha Generator/script.js
+++ Recaptcha Generator/script.js
@@ -1,43 +1,40 @@
-const recaptcha = document.querySelector('.js-recaptcha');
-const inputValue = document.querySelector('.js-input');
-const result = document.querySelector('.js-result');
-const refresh = document.querySelector('.js-refresh');
-const check = document.querySelector('.js-check');
+const recaptcha = document.querySelector(".js-recaptcha");
+const inputValue = document.querySelector(".js-input");
+const result = document.querySelector(".js-result");
+const refresh = document.querySelector(".js-refresh");
+const check = document.querySelector(".js-check");
 
 generateRecaptcha();
 
-function generateRecaptcha(){
-  const info = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
+function generateRecaptcha() {
+  const info = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
   let captchaLength = 6;
-  let captcha = '';
-  
-  for (let i = 0; i < captchaLength;i++) {
-    const randomIndex = Math.floor(Math.random() * info.length);
+  let captcha = "";
+
+  for (let i = 0; i < captchaLength; i++) {
+    const randomIndex = Math.floor(Math.random() * info.length) + 1;
     captcha += info.charAt(randomIndex);
   }
   recaptcha.innerHTML = captcha;
-  inputValue.value = '';
-  result.innerHTML = '';
+  inputValue.value = "";
+  result.innerHTML = "";
 }
 
-
-refresh.addEventListener('click', () => {
+refresh.addEventListener("click", () => {
   generateRecaptcha();
 });
 
-function checkRecaptcha(){
-
+function checkRecaptcha() {
   const userCaptcha = inputValue.value;
-  const captcha = recaptcha.innerHTML
-  if( userCaptcha === captcha){
-    result.innerHTML = 'Captcha is Correct!!';
+  const captcha = recaptcha.innerHTML;
+  if (userCaptcha === captcha) {
+    result.innerHTML = "Captcha is Correct!!";
   } else {
-    result.innerHTML = 'Incorrect Captcha!!';
+    result.innerHTML = "Incorrect Captcha!!";
   }
 }
 
-check.addEventListener('click', () => {
+check.addEventListener("click", () => {
   checkRecaptcha();
 });
-
